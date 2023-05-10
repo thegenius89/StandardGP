3f297cb2234d0be40f9dc756a295be7e68978133
Thure Foken <thurefoken@gmail.com>
2023-05-10 15:41:36 +0200

    Update TreeFunction.py

--- TreeFunction.py
+++ TreeFunction.py
@@ -10,21 +10,22 @@ from Helper import sq, psqrt, plog, pexp, pdiv
 
 class TreeFunction:
 
-    def __init__(self, cfg, function: str, n: int):
+    def __init__(self, x: ndarray, y: ndarray, cfg):
+        self.x_train = x.T
+        self.y_train = y
         self.cfg = cfg
-        self.repr = function
         self.namespace = {
-            "v": rand(n) * 4 - 2, "w": rand(n) * 4 - 2, "x": rand(n) * 4 - 2,
-            "y": rand(n) * 4 - 2, "z": rand(n) * 4 - 2, "sin": sin, "cos": cos,
+            "sin": sin, "cos": cos, "tan": tan, "abs": abs, "pi": pi,
             "exp": pexp, "div": pdiv, "sqrt": psqrt, "pih": pi * 0.5,
-            "log": plog, "sq": sq, "neg": negative, "abs": abs, "pi": pi,
-            "tan": tan,
+            "log": plog, "sq": sq, "neg": negative,
         }
-        self.size = n * 1.0
-        self.tar = self.eval_expr(function)
+        for input in range(self.x_train.shape[0]):
+            self.namespace["x{}".format(input)] = self.x_train[input]
+        self.size = self.x_train.shape[1] * 1.0
+        self.tar = self.normalize(y)
 
-    def eval_expr(self, repr: str) -> ndarray:
-        pred = eval(repr, self.namespace)
+    def normalize(self, pred: ndarray) -> ndarray:
+        # normalize dataset around 0
         pred = pred - add.reduce(pred) / self.size
         pred += rand(int(self.size)) * self.cfg.noise
         pred = pred / (add.reduce(pred * pred) ** 0.5)
@@ -34,9 +35,9 @@ class TreeFunction:
         try:
             from scipy.stats import linregress
             x = eval(repr, self.namespace)
-            y = eval(self.repr, self.namespace)
+            y = self.y_train
             slope, intercept, _r, _p, _std_err = linregress(x, y)
             slope, intercept = round(slope, 8), round(intercept, 8)
             return "{} + {} * ({})".format(intercept, slope, repr)
         except Exception as ex:
-            return str(ex)
+            return repr
