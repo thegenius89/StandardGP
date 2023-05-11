1e847b34ebf3bc32a5e31bfc2b5f7f54468b81e0
Thure Foken <thurefoken@gmail.com>
2023-05-11 17:14:16 +0200

    Update Example.py

--- Example.py
+++ Example.py
@@ -12,10 +12,10 @@ if __name__ == "__main__":
     # y = x[:, 0] ** 4 - x[:, 0] ** 3 + 0.5 * x[:, 1] ** 2 - x[:, 1]
     # y = x[:, 0] * x[:, 1] + np.sin((x[:, 0] - 1) * (x[:, 1] - 1))
     # y = x[:, 0] ** 3 + x[:, 1] ** 3 - x[:, 1] - x[:, 0]
-    # Even more complicated functions
+    # even more complicated functions
     # x = np.random.rand(200, 5) * 2
     # y = ((np.tan(x[:, 0]) / np.exp(x[:, 1])) * (np.log(x[:, 2]) - np.tan(x[:, 3])))
     gp = GP(x, y)
-    best_fit, model = gp.run(show=True)
+    best_fit, model = gp.run(show=True, threads=8)
     print("Epochs: {}, Fit: {}, Model: {}".format(gp.gen, best_fit, model))
     print("RMSE:", np.sqrt(np.mean(np.square(gp.predict(x) - y))))
