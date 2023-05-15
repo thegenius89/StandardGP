ff4ae842793bf9966356caf556475295459fee83
Thure Foken <thurefoken@gmail.com>
2023-05-15 12:53:23 +0200

    Delete Trees2Matrix.py

--- Trees2Matrix.py
+++
@@ -1,5 +0,0 @@
-# Trees2Matrix is an approach to densly pack many trees
-# in a matrix to perform vectorized operatins on many
-# trees at the same time without wasting much memory
-
-import numpy as np
